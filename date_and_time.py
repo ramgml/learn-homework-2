fd57487eb9194b97ad9d61f6adbdca808b8860c3
Yuriy Tyukalov <ramgml@gmail.com>
2020-02-27 16:11:59 +0400

    Задание про datetime

--- date_and_time.py
+++ date_and_time.py
@@ -8,21 +8,27 @@
 * Превратите строку "01/01/17 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, date
+from dateutils import relativedelta
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
-    
+    print(date.today() - relativedelta(days=1))
+    print(date.today())
+    print(date.today() - relativedelta(months=1))
+
+
 def str_2_datetime(string):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    return datetime.strptime(string, '%d/%m/%y %I:%M:%S.%f')
+
 
 if __name__ == "__main__":
     print_days()
